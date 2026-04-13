9688d7dbc9f8b48cbcc91e322e5535c22087852e
Souheab <souheab@protonmail.com>
2026-04-13 17:41:24 -0400

    delete c wm

--- wm/scripts/build_nix.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i bash -p xorg.libX11 xorg.libXft xorg.libXinerama fontconfig freetype pkg-config gcc gnumake
-
-# Build script for sadewm using nix-shell
-# This allows compilation regardless of host operating system environment
-
-echo "Building sadewm with Nix environment..."
-make "$@"
